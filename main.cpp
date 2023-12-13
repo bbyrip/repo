a281ee80e72042e7cb02194b8d968c5d3d4ad261
rptrm <riptrudmay@mail.ru>
2023-12-14 01:17:37 +0300

    Two

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
-#include "std.h"
+#include "sdt.h"
 
 int main() {
+
+    int num1, num2;
+
+    printf("Enter two number: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Enter: %d and %d\n", num1, num2);
 
     return 0;
 }
